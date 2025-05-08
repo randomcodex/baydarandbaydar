64703ec95b48cc64763ed0d899cc939328e84954
randomcodex <barishbaydar@gmail.com>
2025-05-08 03:08:39 +0300

    update

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
 
 export default defineConfig({
-  base: '/baydarandbaydar/',
+  base: '/',
   plugins: [react()],
   build: {
     rollupOptions: {
