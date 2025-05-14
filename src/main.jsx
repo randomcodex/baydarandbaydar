f01fbd32b7b12b6a05203e57feefb128c362d024
randomcodex <barishbaydar@gmail.com>
2025-05-14 22:13:16 +0300

    Netlified

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import './index.css';
+import './global.css'; // Updated to reflect the renamed file
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // Wraps the application in BrowserRouter for client-side routing
