87b81b8399ad90f482aabdc410f8ae3e17c162c7
randomcodex <156702145+randomcodex@users.noreply.github.com>
2026-02-17 20:26:24 +0200

    feat: update

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import './Logo.scss'
 
@@ -9,8 +9,15 @@ interface LogoProps {
 
 const Logo: React.FC<LogoProps> = ({ className = '', size = 'large' }) => {
   const navigate = useNavigate()
+  const [isAnimating, setIsAnimating] = useState(false)
 
   const handleLogoClick = () => {
+    // Trigger pulse animation on mobile and tablet (below 1024px)
+    if (window.innerWidth < 1024) {
+      setIsAnimating(true)
+      setTimeout(() => setIsAnimating(false), 1500)
+    }
+
     navigate('/')
 
     window.scrollTo({ top: 0, behavior: 'smooth' })
@@ -18,7 +25,7 @@ const Logo: React.FC<LogoProps> = ({ className = '', size = 'large' }) => {
 
   return (
     <div
-      className={`logo ${className} logo--${size}`}
+      className={`logo ${className} logo--${size} ${isAnimating ? 'logo--pulse' : ''}`}
       onClick={handleLogoClick}
       role='button'
       tabIndex={0}
@@ -31,7 +38,7 @@ const Logo: React.FC<LogoProps> = ({ className = '', size = 'large' }) => {
       aria-label='Go to home page'
     >
       <img
-        src='./assets/images/header/android-launchericon-96-96.png'
+        src='./assets/images/header/android-launchericon-96-96.svg'
         alt='Baydar & Baydar Logo'
         className='logo__image'
         width={96}
